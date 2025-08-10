76d3576d0f0eac182aa7d4c21ad13badcbc2d737
Amitreddy14 <jsammureddy140804@gmail.com>
2025-08-11 00:08:49 +0530

    Order lines mapping functions with and without clustering

--- utils/cluster/clustering.py
+++ utils/cluster/clustering.py
@@ -44,3 +44,55 @@ def cluster_wave(df, distance_threshold, dist_method, clust_start, df_type):
     # List_coord
     list_coord = np.stack(list_coord)
     return list_coord, list_OrderNumber, clust_id, df
+
+
+def lines_mapping(df, orders_number, wave_start):
+    '''Step 4: Mapping Order lines mapping without clustering '''
+    # Unique order numbers list
+    list_orders = df.OrderNumber.unique()
+    # Dictionnary for mapping
+    dict_map = dict(zip(list_orders, [i for i in range(1, len(list_orders))]))
+    # Order ID mapping
+    df['OrderID'] = df['OrderNumber'].map(dict_map)
+    # Grouping Orders by Wave of orders_number 
+    df['WaveID'] = (df.OrderID%orders_number == 0).shift(1).fillna(0).cumsum() + wave_start
+    # Counting number of Waves
+    waves_number = df.WaveID.max() + 1
+    return df, waves_number
+
+
+def lines_mapping_clst(df, list_coord, list_OrderNumber, clust_id, orders_number, wave_start):
+    '''Step 4: Mapping Order lines mapping with clustering '''
+    # Dictionnary for mapping by cluster
+    dict_map = dict(zip(list_OrderNumber, clust_id))
+    # Dataframe mapping
+    df['ClusterID'] = df['OrderNumber'].map(dict_map)
+    # Order by ID and mapping
+    df = df.sort_values(['ClusterID','OrderNumber'], ascending = True)
+    list_orders = list(df.OrderNumber.unique())
+    # Dictionnary for order mapping 
+    dict_omap = dict(zip(list_orders, [i for i in range(1, len(list_orders))]))
+    # Order ID mapping
+    df['OrderID'] = df['OrderNumber'].map(dict_omap)
+    # Create Waves: Increment when reaching orders_number or changing cluster
+    df['WaveID'] = wave_start + ((df.OrderID%orders_number == 0) | (df.ClusterID.diff() != 0)).shift(1).fillna(0).cumsum() 
+
+    wave_max = df.WaveID.max()
+    return dict_map, dict_omap, df, wave_max
+
+
+def locations_listing(df_orderlines, wave_id):
+    ''' Step 5: Listing location per Wave of orders'''
+
+    # Filter by wave_id
+    df = df_orderlines[df_orderlines.WaveID == wave_id]
+    # Create coordinates listing
+    list_coord = list(df['Coord'].apply(lambda t: literal_eval(t)).values) 	# Here we use Coord for distance
+    list_coord.sort()
+    # Get unique Unique coordinates
+    list_coord = list(k for k,_ in itertools.groupby(list_coord))
+    n_locs = len(list_coord)
+    n_lines = len(df)
+    n_pcs = df.PCS.sum()
+
+    return list_coord, n_locs, n_lines, n_pcs
